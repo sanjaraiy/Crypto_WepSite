3dfbee0c4b5b21861bccad13750017e277d3573e
sanjaraiy <deepakrai78788@gmail.com>
2024-02-19 20:19:05 +0530

    done transaction page for desktop

--- src/Pages/Transaction/Transaction.jsx
+++ src/Pages/Transaction/Transaction.jsx
@@ -1,30 +1,30 @@
-import { Flex, Button, Card, Icon } from "@chakra-ui/react";
+import { Flex, Button, Card, Icon, HStack } from "@chakra-ui/react";
 import React from "react";
 import DashboardLayout from "../../components/DashboardLayout";
 import { MdOutlineFileDownload } from "react-icons/md";
-import { Tabs, TabList, TabPanels, Tab, TabPanel,Tag } from '@chakra-ui/react'
-
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Tag,InputGroup, InputLeftElement,Input } from "@chakra-ui/react";
+import { IoSearch } from "react-icons/io5"
 import TransactionTable from "./components/TransactionTable";
 
 function TransactionPage() {
-   const tabs=[
+  const tabs = [
     {
-      name:"All",
-      count:349,
+      name: "All",
+      count: 349,
     },
     {
-      name:"Deposit",
-      count:114,
+      name: "Deposit",
+      count: 114,
     },
     {
-      name:"Withdraw",
-      count:213,
+      name: "Withdraw",
+      count: 213,
     },
     {
-      name:"Trade",
-      count:22,
-    }
-   ]
+      name: "Trade",
+      count: 22,
+    },
+  ];
   return (
     <DashboardLayout>
       <Flex justify="end" mt="6" mb="3">
@@ -34,19 +34,29 @@ function TransactionPage() {
       </Flex>
       <Card>
         <Tabs>
-          <TabList pt="4">
-          {
-            tabs.map((tab)=>(
-              <Tab key={tab.name} display="flex" gap="2">
-                {tab.name} <Tag colorScheme="gray" borderRadius="full">{tab.count}</Tag>
+          <TabList pt="4" display="flex" justifyContent="space-between">
+           <HStack>
+           {tabs.map((tab) => (
+              <Tab key={tab.name} display="flex" gap="2" pb="4">
+                {tab.name}{" "}
+                <Tag colorScheme="gray" borderRadius="full">
+                  {tab.count}
+                </Tag>
               </Tab>
-            ))
-          }
-            
+            ))}
+           </HStack>
+
+            <InputGroup maxW="250px" pr="6">
+              <InputLeftElement pointerEvents="none">
+                <Icon as={IoSearch}></Icon>
+              </InputLeftElement>
+              <Input type="tel" placeholder="Search here..." />
+            </InputGroup>
+
           </TabList>
           <TabPanels>
             <TabPanel>
-               <TransactionTable></TransactionTable>
+              <TransactionTable></TransactionTable>
             </TabPanel>
             <TabPanel>
               <p>two!</p>
--- src/Pages/Transaction/components/TransactionTable.jsx
+++ src/Pages/Transaction/components/TransactionTable.jsx
@@ -3,125 +3,149 @@ import {
     Table,
     Thead,
     Tbody,
-    Tfoot,
+    Text,
     Tr,
     Th,
     Td,
-    TableCaption,
+    
     TableContainer,
     Stack,
+    Tag
   } from '@chakra-ui/react'
 function TransactionTable() {
     
-    const tableData=[
-       {
-        id:"HD82NA2H",
-        date:"2023-06-20",
-        time:"07:00 AM",
-        type:{
-            name:"INR Deposit",
-            tag:"E-Transfer"
-        },
-        amount:"+₹81,123",
-        status:"Pending",
-       },
-       {
-        id:"HD82NA2H",
-        date:"2022-06-09",
-        time:"02:48 PM",
-        type:{
-            name:"INR Withdraw",
-            tag:"Wire Transfer",
-        },
-        amount:"-₹81,123",
-        status:"Processing",
-
-       },
-       {
-        id:"HD82NA2H",
-        date:"2022-06-04",
-        time:"09:21 AM",
-        type:{
-            name:"Buy",
-           
-        },
-        amount:"+12.485133 BTC",
-        status:"Cancelled",
-       },
-       {
-        id:"HD82NA2H",
-        date:"2022-06-03",
-        time:"01:32 PM",
-        type:{
-          name:"Sell",
-        },
-        amount:"-0.364016 BTC",
-        status:"Completed",
-       },
-       {
-        id:"HD82NA2H",
-        date:"2022-06-03",
-        time:"01:32 PM",
-        type:{
-          name:"BTC Deposit",
-        
-        },
-        amount:"+4.1394616 BTC",
-        status:"Completed",
-       },
-       {
-        id:"HD82NA2H",
-        date:"2022-06-03",
-        time:"01:32 PM",
-        type:{
-          name:"BTC Withdraw",
-         
-        },
-        amount:"-10.0000000 BTC",
-        status:"Completed",
-       },
-    ]
-
-  return (
-    <TableContainer>
-    <Table variant='simple'>
-      <TableCaption>Imperial to metric conversion factors</TableCaption>
-      <Thead>
-        <Tr>
-          <Th>ID</Th>
-          <Th>Date & Time</Th>
-          <Th isNumeric>Type</Th>
-          <Th isNumeric>Amount</Th>
-          <Th isNumeric>Status</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-      {
-        tableData.map(data=>(
-          <Tr key={data.id}>
-            <Td>{data.id}</Td>
-            <Td>
-              <Stack>
-                <Text>{data.date}</Text>
-                <Text>{data.time}</Text>
-              </Stack>
-            </Td>
-            <Td>
-            {" "}
-            <Stack>
-              <Text>{data.type.name}</Text>
-              <Text>{data.type?.tag}</Text>
+  const tableData = [
+    {
+      id: "HD82NA2H",
+      date: "2023-06-20",
+      time: "07:00 AM",
+      type: {
+        name: "INR Deposit",
+        tag: "E-Transfer",
+      },
+      amount: "+₹81,123",
+      status: "pending",
+    },
+    {
+      id: "HD82NA4H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "INR Widthdraw",
+        tag: "Wire Transfer",
+      },
+      amount: "-₹55,123",
+      status: "processing",
+    },
+    {
+      id: "HD82NA5H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "Buy",
+        tag: "BTC",
+      },
+      amount: "12.0554484 BTC",
+      status: "cancelled",
+    },
+    {
+      id: "HD82NA6H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "Sell",
+        tag: "BTC",
+      },
+      amount: "-2.0554484 BTC",
+      status: "completed",
+    },
+    {
+      id: "HD82NA7H",
+      date: "2023-06-20",
+      time: "07:00 AM",
+      type: {
+        name: "BTC Deposit",
+      },
+      amount: "+15.5000000",
+      status: "pending",
+    },
+    {
+      id: "HD82NA8H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "BTC Widthdraw",
+      },
+      amount: "-5.05555544",
+      status: "completed",
+    },
+  ];
+  
+  const statusColor = {
+    pending: "#797E82",
+    processing: "#F5A50B",
+    completed: "#059669",
+    cancelled: "#DC2626",
+  };
+  
+return (
+  <TableContainer>
+  <Table variant="simple" colorScheme="gray">
+    <Thead>
+      <Tr>
+        <Th>ID</Th>
+        <Th>Date & Time</Th>
+        <Th>Type</Th>
+        <Th>Amount</Th>
+        <Th>Status</Th>
+      </Tr>
+    </Thead>
+    <Tbody color="p.black">
+      {tableData.map((data) => (
+        <Tr key={data.id}>
+          <Td fontSize="sm" fontWeight="medium">
+            {data.id}
+          </Td>
+          <Td>
+            <Stack spacing={0}>
+              <Text fontSize="sm" fontWeight="medium">
+                {data.date}
+              </Text>
+              <Text fontSize="xs" color="black.60">
+                {data.time}
+              </Text>
             </Stack>
-            </Td>
-            <Td>{data.amount}</Td>
-            <Td>{data.status}</Td>
-          </Tr>
-        ))
-      }
-      </Tbody>
-    </Table>
-  </TableContainer>
-  )
+          </Td>
+          <Td>
+            {" "}
+            <Stack spacing={0}>
+              <Text fontSize="sm" fontWeight="medium">
+                {data.type.name}
+              </Text>
+              <Text fontSize="xs" color="black.60">
+                {data.type?.tag}
+              </Text>
+            </Stack>
+          </Td>
+          <Td fontSize="sm" fontWeight="medium">
+            {data.amount}
+          </Td>
+          <Td fontSize="sm" fontWeight="medium">
+            <Tag
+              bg={statusColor[data.status]}
+              color="white"
+              borderRadius="full"
+            >
+              {data.status}
+            </Tag>
+          </Td>
+        </Tr>
+      ))}
+    </Tbody>
+  </Table>
+</TableContainer>
+);
+
 }
 
 export default TransactionTable
